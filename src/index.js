ce463ff9cde73b016431fbe373bb1d2e1036a105
Ben <bwchancellor@gmail.com>
2017-07-10 18:52:28 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import Stripe from './components/Stripe';
 
 //var images = ["images/spore1.png", "images/spore2.png", "images/spore3.png", "images/spore4.png", "images/spore5.png"]
 
+//uncomment the ReactDOM.render function below + images variable above to view react image slider (but remember to comment out the Stripe ReactDOM.render function)
 //ReactDOM.render(<Slider images={images}/>, document.getElementById('component'));
 
 ReactDOM.render(<Stripe />, document.getElementById('component'));
